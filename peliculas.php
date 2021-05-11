547369bdd990dee6d45d0d37f63463d0cdd38450
yaizalpz <56633739+yaizalpz@users.noreply.github.com>
2021-05-11 17:08:36 +0200

    Update peliculas.php

--- peliculas.php
+++ peliculas.php
@@ -8,12 +8,53 @@ function mostrarPeliculas() {
 	if (isset($_SESSION["login"]) && ($_SESSION["login"]===true) && ($_SESSION["esGestor"] == true || $_SESSION["esAdmin"] == true)) {
 		$result .= '<a href="nuevaPelicula.php">Añadir película</a>';
 	}
-	$result .= $peliculas->listaPeliculas();
+	
+
+		if(isset($_POST['desplegable'])){
+		$tipoorden= $_POST['desplegable'];
+		switch ($tipoorden) {
+		case 1:
+			$result .= $peliculas->listaPeliculas();
+			break;
+		case 2:
+			$result .= $peliculas->listaPeliculasFecha();
+			break;
+		case 3:
+			$result .= $peliculas->listaPeliculasRating();
+			break;
+		case 4:
+			$result .= $peliculas->listaPeliculasDuration();
+			break;
+}
+		
+	}
+	else{
+		$result .= $peliculas->listaPeliculas();
+		
+	}
+
 	return $result;
+}
+function dropdown_ordenacion() {
+	
+	
+	$html='<form action="" method="post">';
+
+  $html.='<select name="desplegable">';
+    $html.='<option value="1">Por orden alfabético</option>';
+    $html.='<option value="2">Por fecha de estreno</option>';
+    $html.='<option value="3">Por valoración</option>';
+	$html.='<option value="4">Por duración</option>';
+	$html.='</select>';
+	
+$html.='<input type="submit" value="Submit">	';
+$html.= '</form>';
+return $html;
 }
 
 $tituloPagina = 'Películas';
+$contenidoPrincipal= dropdown_ordenacion();
+$contenidoPrincipal.=mostrarPeliculas();
 
-$contenidoPrincipal=mostrarPeliculas();
 
 require __DIR__ . '/includes/plantillas/plantilla.php';
